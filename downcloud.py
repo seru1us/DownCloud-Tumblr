f00175d419fd66e0e2f5e8efbaa735a16bfa2c9c
Sean McCully <seancmccully@gmail.com>
2018-12-04 11:02:20 -0500

    removed NSFW option, and added try clause to catch any malformed post titles.

--- downcloud.py
+++ downcloud.py
@@ -12,8 +12,9 @@ reddit = praw.Reddit(client_id=praw_client_id,
                      user_agent=praw_user_agent,
                      username=praw_username)
 
-#subarg = reddit.subreddit(sys.argv[1])
-subarg = "badwomensanatomy"
+subarg = reddit.subreddit(sys.argv[1])
+strarg = str(subarg)
+
 
 slash = '/'
 underscore = '_'
@@ -21,28 +22,31 @@ period = '.'
 
 # for what it's worth, there is probably a better way to do all of this split/join nonsense.
 
-if not os.path.exists(subarg):
-    os.makedirs(subarg)
-    print('Artist ' + subarg + ' not found, creating...')
+if not os.path.exists(strarg):
+    os.makedirs(strarg)
+    print(strarg + ' not found, creating...')
 else:
-    print('Artist ' + subarg + ' already exists, skipping...')
+    print(strarg + ' already exists, skipping...')
 
-if not os.path.exists(subarg + '/Single Images'):
-    os.makedirs(subarg + '/Single Images')
-    print('Artist ' + subarg + '/Single Images not found, creating...')
+if not os.path.exists(strarg + '/Single Images'):
+    os.makedirs(strarg + '/Single Images')
+    print(strarg + '/Single Images not found, creating...')
 else:
-    print('Artist ' + subarg + '/Single Images already exists, skipping...')
+    print(strarg + '/Single Images already exists, skipping...')
 
 # search the sub for tumblr related links.
-for submission in reddit.subreddit(str(subarg)).search(f'site:tumblr.com nsfw:{praw_sub_nsfw}', sort='relevance', syntax='lucene', time_filter='all', limit=None):
+for submission in reddit.subreddit(strarg).search(f'site:tumblr.com', sort='relevance', syntax='lucene', time_filter='all', limit=None):
 
-    print(submission.url)
+    print("Attempting to download " + submission.url)
     # save "only images" if we want to. 
     if "media" in slash.join(submission.url.split(slash)[:3]) and save_cdn_only_links:
         # take the url and force the highest resolution (this is kinda dirty).
         cdn_url = underscore.join(submission.url.split('_')[:-1]) + '_1280.' + period.join(submission.url.split('.')[-1:])
         # Saves this into the "Single Images" folder, with the title the submission title.
-        urllib.request.urlretrieve(cdn_url, os.path.join(subarg + '/Single Images', submission.title))
+        try:
+            urllib.request.urlretrieve(cdn_url, os.path.join(strarg + '/Single Images', submission.title))
+        except:
+            print("This title is not formatted well, not saving this image.")
     else:
         # so it isn't a link to a pic. Grab the blog name and pass it to the script
         del sys.argv[0]
